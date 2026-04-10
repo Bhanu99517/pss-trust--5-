d4feaeea77d89e405c414c99f876a0a90ebd6931
S. Praveen Kumar <kumarpraveen27978@gmail.com>
2026-04-10 19:09:03 +0530

    Update SuccessStories.tsx

--- src/pages/SuccessStories.tsx
+++ src/pages/SuccessStories.tsx
@@ -3,6 +3,7 @@ import { motion } from 'motion/react';
 import { Quote, GraduationCap, Briefcase, Award } from 'lucide-react';
 
 const stories = [
+
   {
     name: "Akula Kalyani",
     education: "JNTUH Campus-ECE",
@@ -16,19 +17,7 @@ const stories = [
     maritalStatus: "Married, Husband working as Software Engineer",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/976fd28d-8f32-435a-acea-0d4333088197.jpg"
   },
-  {
-    name: "P Chamanthi ",
-    education: " IIIT Basara-ECE",
-    fatherProfession: "Father is a Daily wages worker",
-    joinedYear: "2007",
-    studied: "10th, Intermediate, and B.Tech",
-    company: "Pega Systems India",
-    designation: " Software Engineer 2",
-    package: "26 LPA",
-    siblings: " 1 Younger sister and 1 Elder sister",
-    maritalStatus: " Single",
-    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/63e678fc-7588-4fa1-8e27-26c5eb0f6482.jpg"
-  },
+      // 1
   {
     name: "AKULA MOUNIKA ",
     education: "9th, 10th, Diploma & B.Tech",
@@ -42,6 +31,7 @@ const stories = [
     maritalStatus: " Married",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/1ed7b5aa-b66f-44c3-9447-5e8bb875a2c9.png"
   },
+      // 2
    {
     name: "Ala Sravanthi",
     education: "B.Tech CBIT - ECE",
@@ -55,6 +45,7 @@ const stories = [
     maritalStatus: "Married, Husband working as Software Engineer",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8b530164-279c-4a82-b164-6ddfa02bcb1b.png"
   },
+  // 3
    {
     name: "Ganna Swathi",
     education: "B.Tech CVSR - CIVIL",
@@ -68,6 +59,7 @@ const stories = [
     maritalStatus: "Married, Husband working as Software Engineer",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/a026f84a-864a-4f5d-b23e-dee845a5d9f0.png"
   },
+  // 4
   {
     name: "M. Swapna",
     education: "B.Tech CBIT - ECE",
@@ -81,6 +73,7 @@ const stories = [
     maritalStatus: "Married, Husband working as Software Engineer",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/e31b1d5a-3acd-475d-a42c-1e55ed1c047b.png"
   },
+      // 5
   {
     name: "MUDHIGUNDA SANGEETHA",
     education: "9th, 10th, Diploma & B.Tech",
@@ -94,6 +87,7 @@ const stories = [
     maritalStatus: "Married",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/55a4a666-2981-4bdc-8737-e5a5b966b211.png"
   },
+      // 6
   {
     name: "Sharanamma",
     education: "B.Tech DRK - ECE",
@@ -106,6 +100,196 @@ const stories = [
     maritalStatus: " Married, Husband working in private company",
     image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8a6a2556-bf36-46b5-9d23-656807e35200.png"
   },
+  // 7
+  {
+    name: "PUJARI NAGALAXMI",
+    education: "Engineering at VNR VJIET (ECE) JNTU",
+    fatherProfession: "Daily Wage Worker",
+    joinedYear: "2005",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "SR.ENGG @ TCS",
+    package: " 12 LPA",
+    siblings: " 2 Younger Sisters",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/1c322a1f-0a20-46a5-b404-d94451f8274c.png"
+  },
+  // 8
+  {
+    name: "DOLA KUMARI",
+    education: "Engineering at GRIET (CIVIL) JNTU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "10th, Intermediate, and B.Tech",
+    company: "Self employed",
+    package: "6.2 LPA",
+    siblings: "  1 Younger brother",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/d021d2b9-df0d-4596-9121-c6e04b3a8e5a.png"
+  },
+  // 9
+ {
+    name: "P Chamanthi ",
+    education: " IIIT Basara-ECE",
+    fatherProfession: "Father is a Daily wages worker",
+    joinedYear: "2007",
+    studied: "10th, Intermediate, and B.Tech",
+    company: "Pega Systems India",
+    designation: " Software Engineer 2",
+    package: "26 LPA",
+    siblings: " 1 Younger sister and 1 Elder sister",
+    maritalStatus: " Single",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8d9a38bc-c471-49b8-ab12-69df98b2efa2.png"
+  },
+  // 10
+  {
+    name: "AKULA MOUNIKA",
+    education: "CSE",
+    fatherProfession: "Auto Driver",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "SR.ENGG @ AMD",
+    package: "26 LPA",
+    siblings: "1 Younger Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/2b01a171-ab6e-4c90-b1e6-9df3f998795f.png"
+  },
+  // 11
+  {
+    name: "MD BHASHEERA",
+    education: "Engineering at SNIST (ECE) JNTU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "10th, diploma, and B.Tech",
+    company: "Self employed",
+    package: " 12 LPA",
+    siblings: "1 Younger Brother & 1 Elder Sisterr",
+    maritalStatus: "Single",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/a8590aec-6694-44da-84d6-ca90b19ac299.png"
+  },
+  // 12
+  {
+    name: "ALETI CHANDRASHEKAR",
+    education: "Engineering at CMR (ECE) JNTU",
+    fatherProfession: "Auto Driver",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "SC @ PALO ALTONETWORKS ATSINGAPORE",
+    package: "45 LPA",
+    siblings: "1 Elder Sister & 1 Younger Brother",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/5ad856bb-3b82-4871-9823-19f1455facd4.png"
+  },
+  // 13
+  {
+    name: "KONDAPALLY SWATHI",
+    education: "Engineering at CMR (ECE)JNTU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "10th, diploma, and B.Tech",
+    company: "SW.ENGG @ CONTROL X",
+    package: " 6.5 LPA",
+    siblings: " 1 Younger Brother & 1 Younger Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/6d74a363-5220-40a8-b15c-bb4325b4944e.png"
+  },
+  // 14
+  {
+    name: "CHINMAMITI VANI",
+    education: "Bellevue College of Nursing",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "9th, 10th, Inter Nursing",
+    company: "Self employed",
+    package: "4.2 LPA",
+    siblings: "1 Elder Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/841e6330-ae90-4993-b69a-60c93eb4079c.png"
+  },
+  // 15
+  {
+    name: "DOLA PALGUNA",
+    education: "Engineering at VNR VJIET (CIVIL) JNTUE",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "10th, diploma , and B.Tech",
+    company: "QS @ AVS CONSTRUCTIONS",
+    package: " 7.6 LPA",
+    siblings: "1 Elder Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/6387d99f-143f-49ac-b272-9fe31bc093c5.png"
+  },
+  // 16
+  {
+    name: "SURYAVAMSHI VIJAYASHANTI",
+    education: "Engineering at DRK (ECE) JNTU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "TEAM LEAD @ TCS",
+    package: "12 LPA",
+    siblings: "1 Elder Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8a6a2556-bf36-46b5-9d23-656807e35200.png"
+  },
+  // 17
+  {
+    name: "KUMMARI BHARATHI",
+    education: "Engineering at BVRIT (ECE) JNTU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "SR.ENGG @ WIPRO",
+    package: " 11 LPA",
+    siblings: "1 Younger Brother & Sister",
+    maritalStatus: "single",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8a6a2556-bf36-46b5-9d23-656807e35200.png"
+  },
+  // 18
+ {
+    name: "REGATI DHANALAXMI",
+    education: "Engineering at GRIET (ECE) JNTU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "SR.ENGG @ VALUE LABS",
+    package: "14 LPA",
+    siblings: "1 Elder Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8a6a2556-bf36-46b5-9d23-656807e35200.png"
+  },
+  // 19
+  {
+    name: "BRAMHAROUTU RAVIKIRAN",
+    education: "Engineering at Vasavi (CSE) OU",
+    fatherProfession: "Father’ is a Daily wage worker",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.E",
+    company: "ANALYST SCRIPT DEV @VALUE LABS",
+    package: "18 LPA",
+    siblings: "1 Elder Brother & 1 Younger Sister",
+    maritalStatus: " Married",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8a6a2556-bf36-46b5-9d23-656807e35200.png"
+  },
+  // 20
+  {
+    name: "DASARI HARINDER",
+    education: "Engineering at GRIET (CSE) JNTU",
+    fatherProfession: "Mason (Deceased)",
+    joinedYear: "2006",
+    studied: "9th, 10th, Diploma & B.Tech",
+    company: "Self employed",
+    package: " 18LPA",
+    siblings: "2 Elder brothers, 1 Younger brother & sister",
+    maritalStatus: "Single",
+    image: "https://wojpyqvcargyffkyxfln.supabase.co/storage/v1/object/public/shared-files/ab6540cd-1441-4b6e-ac1f-0fae25f0185c/8a6a2556-bf36-46b5-9d23-656807e35200.png"
+  },
+  // 21
+  // 22
+  // 23
+  // 24
+  // 24
+  // 25
+  // 26
 ];
 
 export default function SuccessStories() {
